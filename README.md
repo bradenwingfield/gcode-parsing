699a6ff16e887566265ff2975902f0790704b3ff
Braden Wingfield <wingfieldbraden@gmail.com>
2025-01-25 13:49:14 -0500

    Add cura and prusa functionality along with gcode

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # gcode-parsing
+
+
+
+## Resources:
+How to open and read a text file
+https://www.youtube.com/watch?v=DCaKj3eIrro
+
+How to find a word in a string
+https://www.geeksforgeeks.org/python-how-to-search-for-a-string-in-text-files/
+
+How to parse a string
+https://www.freecodecamp.org/news/how-to-parse-a-string-in-python/
--- parsing.py
+++ parsing.py
@@ -1,8 +1,47 @@
+def extract_data(file_path, slicer):
+    with open(file_path, 'r') as open_file:
+        file_data = open_file.readlines()
+
+    bed_temperature_keyword = "M190"
+    hotend_temperature_keyword = "M109"
+
+    if slicer == "Cura":
+        time_keyword = ";TIME"
+        filament_used_keyword = ";Filament used: "
+    else: 
+        time_keyword = "; estimated printing time (normal mode) = "
+        filament_used_keyword = "; filament used [mm] = "
+
+    for row in file_data:
+        if row.find(bed_temperature_keyword) != -1:
+             bed_temperature = row.split('S')[1].split()[0]
+        if row.find(hotend_temperature_keyword) != -1:
+             hotend_temperature = row.split('S')[1].split()[0]
+
+        if slicer == "Cura":
+            if row.find(time_keyword) != -1:
+                time= row.strip(time_keyword).strip()
+            if row.find(filament_used_keyword) != -1:
+                filament_used= row.strip(filament_used_keyword).strip()
+        else: 
+            if row.find(time_keyword) != -1:
+                time = row.strip(time_keyword).strip()
+            if row.find(filament_used_keyword) != -1:
+                filament_used = row.strip(filament_used_keyword).strip()
+        
+    print(f"Bed Temperature: {bed_temperature}°C")
+    print(f"Hotend Temperature: {hotend_temperature}°C")
+    print(f"Estimated Print Time: {time}")
+    print(f"Filament Used: {filament_used} mm")
+    
+
 #use the provided "3DBenchy.gcode" file
-filePath = input("File Path: ")
+file_path = input("Gcode File Path: ")
 
-fileRead = open(filePath, 'r')
-
-fileData = fileRead.readlines()
-
-print(fileData)
+while True:
+    slicer = input("Slicer Software (Cura or Prusa): ")
+    if slicer == "Cura" or slicer == "Prusa":
+        extract_data(file_path, slicer)
+        break
+    else:
+        print("Error: Please enter a valid slicer software.")
